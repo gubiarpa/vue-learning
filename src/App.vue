08d12c3a6f7f41dfb7c6e0a64fda715a419e5a32
gubiarpa <billy.arredondo@pucp.pe>
2021-08-22 22:32:34 -0500

    add counter

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
+  <Counter />
 </template>
 
 <script>
+import Counter from './components/Counter';
 
 export default {
   name: 'App',
   components: {
-    
+    Counter
   }
 }
 </script>
