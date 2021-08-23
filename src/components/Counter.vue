d1aeb3ef5dbade9da5a0a7235eb68d5269303b06
gubiarpa <billy.arredondo@pucp.pe>
2021-08-22 23:09:27 -0500

    difference between computed and method

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -2,16 +2,37 @@
     <h2>Counter</h2>
     <p>{{ counter }}</p>
     <div>
-        {{ counter }}
+        {{ counter }} <sup>2</sup> = {{ squareValue }}
+    </div>
+    <div>
+        {{ counter }} <sup>2</sup> = {{ squareValue }}
+    </div>
+    <div>
+        {{ counter }} <sup>2</sup> = {{ squareValue }}
+    </div>
+    <div>
+        {{ counter }} <sup>2</sup> = {{ squareValue }}
     </div>
 </template>
 
 <script>
 export default {
-    name: 'Wawitash',
+    // name: 'Wawitash',
     data() {
         return {
             counter: 5
+        }
+    },
+    computed: {
+        squareValue() {
+            console.log('squareValue')
+            return this.counter ** 2
+        }
+    },
+    methods: {
+        getSquareValue() {
+            console.log('getSquareValue')
+            return this.counter ** 2
         }
     },
 }
--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 createApp(App).mount('#app')
-Vue.config.devtools = true;
