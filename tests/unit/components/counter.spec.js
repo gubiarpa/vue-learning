51acaef671572a27e2a7f08985fa5bcb22c7a78a
gubiarpa <billy.arredondo@pucp.pe>
2021-09-16 01:05:19 -0500

    test with shallowMount

--- tests/unit/components/counter.spec.js
+++ tests/unit/components/counter.spec.js
@@ -2,9 +2,21 @@ import { shallowMount, mount } from '@vue/test-utils'
 import Counter from '@/components/Counter'
 
 describe('Counter Component', () => {
+    /*
     test('Debe hacer el match con el snapshot', () => {
         const wrapper = shallowMount(Counter)
 
         expect(wrapper.html()).toMatchSnapshot()
     })
+    */
+
+    test('h2 should have the default value "Counter"', () => {
+
+        const wrapper = shallowMount(Counter)
+
+        // expect(wrapper.find('h2').exists()).toBeTruthy()
+        expect(wrapper.find('h2').text()).toBe('Counter')
+
+    })
+
 })
